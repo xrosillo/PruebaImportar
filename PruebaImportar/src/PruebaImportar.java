c1cbc679083f2efe17c661de388d97dfc39459e6
Xavier <Xavier@SFNavarrosillo>
2021-02-23 11:36:48 +0100

    Commit hecho

---
+++ PruebaImportar/src/PruebaImportar.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Xavier
+ *
+ */
+public class PruebaImportar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("voy a aprobar la segunda evaluación");
+
+	}
+
+}
